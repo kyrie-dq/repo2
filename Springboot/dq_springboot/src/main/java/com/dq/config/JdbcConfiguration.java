96e4d6178ac483350012a113a129e597c8d57c18
kyrie <850554285@qq.com>
2019-08-16 18:52:04 +0800

    aaa

---
+++ Springboot/dq_springboot/src/main/java/com/dq/config/JdbcConfiguration.java
@@ -0,0 +1,28 @@
+package com.dq.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+@Configuration
+@EnableConfigurationProperties(JdbcProperties.class)
+public class JdbcConfiguration {
+
+    @Autowired
+    private JdbcProperties jdbcProperties;
+
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(jdbcProperties.getUrl());
+        dataSource.setDriverClassName(jdbcProperties.getDriverClassName());
+        dataSource.setUsername(jdbcProperties.getUsername());
+        dataSource.setPassword(jdbcProperties.getPassword());
+        return dataSource;
+    }
+
+
+}
